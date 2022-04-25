82e3941c5860d571eb3ec1d151ee4d1057ec3733
KingBlackCow <sgs1159@naver.com>
2022-04-25 21:38:44 +0900

    feat: 람다식 기본규칙

--- src/functionalprogramming/Main.java
+++ src/functionalprogramming/Main.java
@@ -4,9 +4,25 @@ import functionalprogramming.utils.Adder;
 
 import java.util.function.Function;
 
+/*
+ * 매개변수의 타입이 유추 가능할 경우 타입 생략 가능
+ * 매개변수가 하나일 경우 괄호 생략 가능
+ * 바로 리턴하는 경우 중괄호 생략 가능
+ */
+
 public class Main{
     public static void main(String[] args) {
-        Function<Integer, Integer> adder = new Adder();
+        //1.
+//        Function<Integer, Integer> adder = new Adder();
+
+        //2.
+//        Function<Integer, Integer> adder = (Integer x) -> {
+//          return x+10;
+//        };
+
+        //3.
+        Function<Integer, Integer> adder = x -> x+10;
+
         int res = adder.apply(5);
         System.out.println(res);
     }
