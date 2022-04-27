96ec270685862bff957c8f1a801afe1e2579f679
KingBlackCow <sgs1159@naver.com>
2022-04-28 00:07:01 +0900

    feat: constructor reference

---
+++ src/functionalprogramming/model/Suv.java
@@ -0,0 +1,11 @@
+package functionalprogramming.model;
+
+public class Suv extends Car {
+    public Suv(String name, String brand) {
+        super(name, brand);
+    }
+
+    public void drive() {
+        System.out.println("Driving a suv " + name + " from " + brand);
+    }
+}
